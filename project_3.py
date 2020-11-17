63bb9d0b779ccf0e81c0b7edc8f2397847e29279
enolan26 <71350274+enolan26@users.noreply.github.com>
2020-11-17 17:24:27 -0500

    Loop  works, need array export
    Still need to use a better unit converter, and export the three variables to an array.

--- project_3.py
+++ project_3.py
@@ -14,6 +14,7 @@ print (df)
 # ATA = (t*L)/(lw +L*(cos)) + rt
 continue_yn = 'y'
 question = 'ask'
+question2 = 'ask2'
 
 print()
 while continue_yn=='y':
@@ -32,9 +33,19 @@ while continue_yn=='y':
                ata = ((t+rt)*l)/(lw+l)
           else:
                ata = ((t+rt)*l)/((lw+l)*cos)
+          nm1 = ata * .113
+          ftlb1 = ata/12
+
           print()
           print(f'Round up to whole number and set torque wrench to: {ata:.2f} in-lbs')
+          print(f'Round up to whole number and set torque wrench to: {nm1:.2f} Nm')
+          print(f'Round up to whole number and set torque wrench to: {ftlb1:.2f} in-lbs')
           print()
+
+          continue_yn = input('Would you like enter new data? (Y, N)?').lower()
+          print()
+          if continue_yn == 'N':
+               break
           
      if continue_yn == "y":
           question = input('Are you using a crowsfoot, extension or both (Y, N)?').upper()
@@ -45,15 +56,25 @@ while continue_yn=='y':
           rt = float(input("Enter measured running torque in inch-lbs: "))
           print()          
           nt = t + rt
+          nm2 = nt * .113
+          ftlb2 = nt/12
           print(f'Set torque wrench to: {nt:.2f} in-lbs')
+          print(f'Set torque wrench to: {nm2:.2f} Nm')
+          print(f'Set torque wrench to: {ftlb2:.2f} ft-lbs')
+          
           print() 
+     
+          continue_yn = input('Would you like enter new data? (Y, N)?').lower()
+          print()
+          if continue_yn == 'N':
+               break
 
 if continue_yn == "y":
      print()
      question = input('Are you using a crowsfoot, extension or both (Y, N)?').upper()
-          
 
-# runs = (f'Round up to whole number and set torque wrench to: {ata:.2f} in-lbs', f'Set torque wrench to: {nt:.2f} in-lbs')
+#Variables to add to array, nt, ata, nm1, nm2, ftlb1, ftlb2)
+# runs = (????)
 
 # results = []
 # for i in range(runs):
