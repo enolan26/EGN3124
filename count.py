d8fa867d294d6760488380bd0b18e56b9126cfba
enolan26 <71350274+enolan26@users.noreply.github.com>
2020-11-29 14:43:13 -0500

    new counter attempt

---
+++ count.py
@@ -0,0 +1,9 @@
+count = 0; # kind of important to start at 0 for an accurate count
+loopStart = 1; # arbitrary
+loopEnd = n!; #arbitrary
+
+while loopstart < loopend
+    count = count + 1; #this increments by 1 each time the loop executes
+    loopStart = loopStart + 1; # 5 is just some arbitrary constant that I chose to increment by
+    end
+      print(count) # this should display 2 for this specific example
